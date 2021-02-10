34b6f0a33b5f077694794f5679cbf431423e7e6a
Tomasz Cieslak <t.cieslak@mail.utoronto.ca>
2021-02-10 18:52:38 -0500

    [THEN-39] Generate thumbnail image

--- fire/src/model/model.ts
+++ fire/src/model/model.ts
@@ -3,32 +3,56 @@ import { WebGLRenderer } from "./webgl";
 
 export class Model {
     renderer: WebGLRenderer;
+    thumbnailCanvas: HTMLCanvasElement;
+    thumbnailCanvasContext: CanvasRenderingContext2D;
+    canvas: HTMLCanvasElement;
+    project: Project;
 
-    constructor(public canvas: HTMLCanvasElement, public project: Project) {
-        this.project = { name: "test", media: [], width: 1920, height: 1080, framerate: 30, currTimestamp: 0, duration: 0 };
-        this.renderer = new WebGLRenderer(canvas, project);
+    constructor() {
+        this.canvas = document.createElement("canvas");
+        this.project = { name: "test", media: [], segments: [], width: 1920, height: 1080, framerate: 30, currTimestamp: 0, duration: 0 };
+        this.renderer = new WebGLRenderer(this.canvas, this.project);
+        this.thumbnailCanvas = document.createElement("canvas");
+        this.thumbnailCanvasContext = this.thumbnailCanvas.getContext("2d") as CanvasRenderingContext2D;
     }
 
-    public addVideo(element: HTMLVideoElement) {
+    public async addVideo(file: File) {
+        let elm = document.createElement("video") as HTMLVideoElement;
+
+        await new Promise<void>((resolve, reject) => {
+            elm.onloadeddata = () => resolve();
+            elm.src = URL.createObjectURL(file);
+        });
+
+        // Generate Thumbnail
+        this.thumbnailCanvas.width = elm.videoWidth;
+        this.thumbnailCanvas.height = elm.videoHeight;
+        this.thumbnailCanvasContext.drawImage(elm, 0, 0);
+
+        let media = {
+            element: elm,
+            file: file,
+            thumbnail: this.thumbnailCanvas.toDataURL(),
+            texture: this.renderer.createTexture()
+        };
+
         let segment: Segment = {
-            media: { element: element },
+            media: media,
             start: 0,
             duration: 0,
             mediaStart: 0,
             keyframes: [],
-            texture: this.renderer.createTexture(),
             properties: {
                 x: 0,
                 y: 0,
-                width: 0,
-                height: 0,
+                width: elm.videoWidth,
+                height: elm.videoHeight,
             },
         }
 
-        this.project.media.push(segment);
-        this.renderer.loadFrame(segment).then(() => {
-            console.log("Sucessfully Loaded Segment Start!");
-        })
+        this.project.media.push(media);
+        this.project.segments.push(segment);
+        console.log("Sucessfully Loaded Segment Thumbnail!");
     }
 
     public async seek(timestamp: number) {
@@ -36,7 +60,7 @@ export class Model {
     }
 
     private async seekVideos() {
-        
+
     }
 
     private updateProperties() {
