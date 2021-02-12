aba5dbce347aca3237009dc4a4b0a412e2ea20b1
Alees Ahmad Goraya <57045509+aleesgoraya@users.noreply.github.com>
2021-02-11 19:26:43 -0500

    [THEN-43] adds about page and react link to page

--- fire/src/routes/about.tsx
+++ fire/src/routes/about.tsx
@@ -1,23 +1,20 @@
-import React from "react";
+import { Link } from "react-router-dom";
+import styles from "./about.module.css";
 
-function About() {
-
+export default function About() {
     return (
-        <div>
+        <div className={styles.container}>
+            <Link to="/"><span className="material-icons">arrow_back</span></Link>
             <h1>About Us</h1>
             <p>We are a group of developers passionate to bring the best products to consumers.</p>
             <p>We are constantly testing our products for bugs and issues</p>
             <p>If you come accross any such bug or just want to give feedback to our team don't hesisate to contact us</p>
+            <br></br>
             <address>
                 <p>Email Address: <a href="mailto:react.editor@gmail.com">react.editor@gmail.com</a></p>
             </address>
 
-
             <p>Version 0.1</p>
         </div>);
-
-
-
 }
 
-export default About;
