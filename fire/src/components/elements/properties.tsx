8238b1d7a582653716b0c191d78e2cc844f30038
Julian de Rushe <julian.derushe@mail.utoronto.ca>
2021-03-04 22:04:47 -0500

    [THEN-64] Add keyframe playback

--- fire/src/components/elements/properties.tsx
+++ fire/src/components/elements/properties.tsx
@@ -1,96 +1,32 @@
 import styles from "./properties.module.css";
 import { ChangeEvent } from "react";
-import { Segment } from "../../model/types";
+import { Segment, SegmentID } from "../../model/types";
 
-export default function Properties(props: {
-    selectedSegment: Segment,
-    updateSegment: (oldSeg: Segment, segment: Segment) => void
+export default function Properties({
+    trackList,
+    selectedSegment,
+    updateSegment,
+}: {
+    trackList: Segment[][],
+    selectedSegment: SegmentID,
+    updateSegment: (id: SegmentID, segment: Segment) => void
 }) {
+    const segment = trackList[selectedSegment.track][selectedSegment.index];
+
     const changeX = (event: ChangeEvent<HTMLInputElement>) => {
-        let segment: Segment = {
-            media: props.selectedSegment.media,
-            start: props.selectedSegment.start,
-            duration: props.selectedSegment.duration,
-            mediaStart: props.selectedSegment.mediaStart,
-            texture: props.selectedSegment.texture,
-            keyframes: [
-                {
-                    start: props.selectedSegment.keyframes[0].start,
-                    x: +event.target.value,
-                    y: props.selectedSegment.keyframes[0].y,
-                    scaleX: props.selectedSegment.keyframes[0].scaleX,
-                    scaleY: props.selectedSegment.keyframes[0].scaleY,
-                },
-            ],
-            track: props.selectedSegment.track
-        };
-        props.updateSegment(props.selectedSegment, segment);
-    };
+        updateSegment(selectedSegment, { ...segment, keyframes: [{ ...segment.keyframes[0], x: +event.target.value, }] });
+    }
 
     const changeY = (event: ChangeEvent<HTMLInputElement>) => {
-        let segment: Segment = {
-            media: props.selectedSegment.media,
-            start: props.selectedSegment.start,
-            duration: props.selectedSegment.duration,
-            mediaStart: props.selectedSegment.mediaStart,
-            texture: props.selectedSegment.texture,
-            keyframes: [
-                {
-                    start: props.selectedSegment.keyframes[0].start,
-                    x: props.selectedSegment.keyframes[0].x,
-                    y: +event.target.value,
-                    scaleX: props.selectedSegment.keyframes[0].scaleX,
-                    scaleY: props.selectedSegment.keyframes[0].scaleY,
-                },
-            ],
-            track: props.selectedSegment.track
-        };
-        props.updateSegment(props.selectedSegment, segment);
+        updateSegment(selectedSegment, { ...segment, keyframes: [{ ...segment.keyframes[0], y: +event.target.value, }] });
     };
 
     const zoomHeight = (event: ChangeEvent<HTMLInputElement>) => {
-
-        let segment: Segment = {
-            media: props.selectedSegment.media,
-            start: props.selectedSegment.start,
-            duration: props.selectedSegment.duration,
-            mediaStart: props.selectedSegment.mediaStart,
-            texture: props.selectedSegment.texture,
-            keyframes: [
-                {
-                    start: props.selectedSegment.keyframes[0].start,
-                    x: props.selectedSegment.keyframes[0].x,
-                    y: props.selectedSegment.keyframes[0].y,
-                    scaleX: props.selectedSegment.keyframes[0].scaleX,
-                    scaleY: +event.target.value,
-                },
-            ],
-            track: props.selectedSegment.track
-        };
-        props.updateSegment(props.selectedSegment, segment);
-
+        updateSegment(selectedSegment, { ...segment, keyframes: [{ ...segment.keyframes[0], scaleY: +event.target.value, }] });
     };
 
     const zoomWidth = (event: ChangeEvent<HTMLInputElement>) => {
-
-        let segment: Segment = {
-            media: props.selectedSegment.media,
-            start: props.selectedSegment.start,
-            duration: props.selectedSegment.duration,
-            mediaStart: props.selectedSegment.mediaStart,
-            texture: props.selectedSegment.texture,
-            keyframes: [
-                {
-                    start: props.selectedSegment.keyframes[0].start,
-                    x: props.selectedSegment.keyframes[0].x,
-                    y: props.selectedSegment.keyframes[0].y,
-                    scaleX: +event.target.value,
-                    scaleY: props.selectedSegment.keyframes[0].scaleY,
-                },
-            ],
-            track: props.selectedSegment.track
-        };
-        props.updateSegment(props.selectedSegment, segment);
+        updateSegment(selectedSegment, { ...segment, keyframes: [{ ...segment.keyframes[0], scaleX: +event.target.value, }] });
     };
 
     return (
@@ -110,7 +46,7 @@ export default function Properties(props: {
                     step="10"
                     placeholder="0"
                     onChange={changeX}
-                    value={props.selectedSegment.keyframes[0].x}
+                    value={segment.keyframes[0].x}
                 />
             </span>
             <span>
@@ -122,7 +58,7 @@ export default function Properties(props: {
                     step="10"
                     placeholder="0"
                     onChange={changeY}
-                    value={props.selectedSegment.keyframes[0].y}
+                    value={segment.keyframes[0].y}
                 />
             </span>
 
@@ -139,7 +75,7 @@ export default function Properties(props: {
                     min="0.1"
                     max="100.0"
                     onChange={zoomWidth}
-                    value={props.selectedSegment.keyframes[0].scaleX}
+                    value={segment.keyframes[0].scaleX}
                 />
             </span>
             <span>
@@ -152,7 +88,7 @@ export default function Properties(props: {
                     min="0.1"
                     max="100.0"
                     onChange={zoomHeight}
-                    value={props.selectedSegment.keyframes[0].scaleY}
+                    value={segment.keyframes[0].scaleY}
                 />
             </span>
         </div>
--- fire/src/model/mediaManager.tsx
+++ fire/src/model/mediaManager.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useRef, useState } from "react";
 import PlaybackController from "./playbackController";
-import { Media, Segment } from "./types";
+import { Media, Segment, SegmentID } from "./types";
 import { WebGLRenderer } from "./webgl";
 
 export default function MediaManager(props: {}) {
@@ -11,7 +11,7 @@ export default function MediaManager(props: {}) {
     const [projectHeight, setProjectHeight] = useState<number>(1080);
     const [projectFramerate, setProjectFrameRate] = useState<number>(30);
     const [projectDuration, setProjectDuration] = useState<number>(0);
-    const [selectedSegment, setSelectedSegment] = useState<Segment | null>(null);
+    const [selectedSegment, setSelectedSegment] = useState<SegmentID | null>(null);
     const [canvasRef, setCanvasRef] = useState<HTMLCanvasElement>(document.createElement("canvas"));
     const [renderer, setRenderer] = useState<WebGLRenderer>(new WebGLRenderer(canvasRef, projectWidth, projectHeight));
 
@@ -19,6 +19,16 @@ export default function MediaManager(props: {}) {
         canvasRef.width = projectWidth;
         canvasRef.height = projectHeight;
     }, [canvasRef, projectHeight, projectWidth]);
+
+    useEffect(() => {
+        let duration = 0;
+        for (const track of trackList) {
+            if (track.length === 0) continue;
+            duration = Math.max(duration, track[track.length - 1].start + track[track.length - 1].duration);
+        }
+
+        setProjectDuration(duration);
+    }, [trackList]);
 
     const thumbnailCanvas = document.createElement("canvas");
     const thumbnailCanvasContext = thumbnailCanvas.getContext("2d") as CanvasRenderingContext2D;
@@ -104,7 +114,6 @@ export default function MediaManager(props: {}) {
             [...trackList[trackNum], segment],
             ...trackList.slice(trackNum + 1),
         ]);
-        setProjectDuration(Math.max(projectDuration, segment.start + segment.duration));
     }
 
     const deleteVideo = (media: Media) => {
@@ -113,116 +122,58 @@ export default function MediaManager(props: {}) {
             return item !== media;
         }));
 
-        let projectDuration = 0;
-        setTrackList(trackList.map((track) => track.filter((segment: Segment) => {
-            if (segment.media !== media) projectDuration = Math.max(projectDuration, segment.start + segment.duration);
-            if (segment === selectedSegment) setSelectedSegment(null);
+        setTrackList(trackList.map((track, trackInd) => track.filter((segment, segmentInd) => {
+            if (segment.media === media &&
+                selectedSegment?.track === trackInd &&
+                selectedSegment?.index === segmentInd) setSelectedSegment(null);
             return segment.media !== media;
         })));
-        setProjectDuration(projectDuration);
     }
 
     const deleteSelectedSegment = () => {
-        if (selectedSegment !== null) {
-            let projectDuration = 0;
-            setTrackList(trackList.map((track) => track.filter((segment: Segment) => {
-                if (segment !== selectedSegment) projectDuration = Math.max(projectDuration, segment.start + segment.duration);
-                if (segment === selectedSegment) segment.media.element.pause();
-                return segment !== selectedSegment;
-            })));
-            setProjectDuration(projectDuration);
-            setSelectedSegment(null);
-        }
+        if (selectedSegment === null) return;
+
+        trackList[selectedSegment.track][selectedSegment.index].media.element.pause();
+        setTrackList([
+            ...trackList.slice(0, selectedSegment.track),
+            [...trackList[selectedSegment.track].slice(0, selectedSegment.index), ...trackList[selectedSegment.track].slice(selectedSegment.index + 1)],
+            ...trackList.slice(selectedSegment.track + 1)
+        ]);
+        setSelectedSegment(null);
     }
     const split = (timestamp: number) => {
-        if (selectedSegment !== null) {
-            let duration = selectedSegment.duration;
-            let track = selectedSegment.track;
-            // start & end is relative to timeline
-            let start = selectedSegment.start;
-            let end = start + duration;
-            let index = getSegement(timestamp, track);
-            // make sure to not split a segment that is not selected
-            if (!(start <= timestamp && end > timestamp)) {
-                return;
-            }
-            if (index !== -1) {
+        if (selectedSegment === null) return;
 
-                // create and change new Segment properties to adjust to timestamp
+        const segment = trackList[selectedSegment.track][selectedSegment.index];
 
-                // TODO: fix object copy???
-                // let newSegment = Object.assign({}, trackList[track][index]);
-                // newSegment.start = timestamp;
-                // newSegment.duration = end-timestamp;
-                // newSegment.mediaStart =  timestamp - start;
+        if (segment.start > timestamp || segment.start + segment.duration < timestamp) return;
 
-                let newSegment: Segment = {
-                    media: selectedSegment.media,
+        setTrackList([
+            ...trackList.slice(0, selectedSegment.track),
+            [
+                ...trackList[selectedSegment.track].slice(0, selectedSegment.index),
+                { ...trackList[selectedSegment.track][selectedSegment.index], duration: timestamp - segment.start },
+                {
+                    media: segment.media,
                     start: timestamp,
-                    duration: end - timestamp,
-                    mediaStart: timestamp - start,
-                    texture: selectedSegment.texture,
-                    // TODO: Deep copy keyframes adjusted to the split
-                    keyframes: selectedSegment.keyframes,
+                    duration: segment.start + segment.duration - timestamp,
+                    mediaStart: timestamp - segment.start,
+                    texture: segment.texture,
+                    keyframes: segment.keyframes,
                     track: selectedSegment.track
-                }
-                // update original Segment properties to prevent overlap
+                },
+                ...trackList[selectedSegment.track].slice(selectedSegment.index + 1)
+            ],
+            ...trackList.slice(selectedSegment.track + 1)
+        ]);
+    }
 
-                // include new Segment to TrackList
-                let newTrackList = [];
-                for (let i = 0; i < trackList.length; i++) {
-                    if (i === track) {
-                        let newSegmentList = [];
-                        for (let j = 0; j < trackList[i].length; j++) {
-                            // TODO: Deep copy keyframes adjusted to the split
-                            if (j === index) {
-                                trackList[i][j].duration = timestamp - start;
-                                newSegmentList.push(trackList[i][j]);
-                                newSegmentList.push(newSegment);
-                            } else {
-                                newSegmentList.push(trackList[i][j]);
-                            }
-                        }
-                        newTrackList.push(newSegmentList);
-                    }
-                    else {
-                        newTrackList.push(trackList[i]);
-                    }
-                }
-                console.log('Old Track: ', trackList);
-                console.log('New Track: ', newTrackList);
-                setTrackList(newTrackList);
-            }
-        }
-    };
-
-    // given specified trackNum, return the index of the segment given timestamp
-    // if no segement exists, return -1
-    const getSegement = (timestamp: number, trackNum: number) => {
-        var index = -1;
-
-        for (let i = 0; i < trackList[trackNum].length; i++) {
-            let duration = trackList[trackNum][i].duration;
-            // start & end is relative to timeline
-            let start = trackList[trackNum][i].start;
-            let end = start + duration;
-            if (start <= timestamp && end > timestamp) {
-                index = i;
-                break;
-            }
-        }
-        return index;
-    };
-
-    const updateSegment = (oldSeg: Segment, newSegment: Segment) => {
-        let projectDuration = 0;
-        setTrackList(trackList.map((track) => track.map((segment: Segment) => {
-            projectDuration = Math.max(projectDuration, (segment === oldSeg ? (newSegment.start + newSegment.duration) : (segment.start + segment.duration)));
-            return segment === oldSeg ? newSegment : segment;
-        })));
-
-        if (oldSeg === selectedSegment) setSelectedSegment(newSegment);
-        setProjectDuration(projectDuration);
+    const updateSegment = (id: SegmentID, newSegment: Segment) => {
+        setTrackList([
+            ...trackList.slice(0, id.track),
+            [...trackList[id.track].slice(0, id.index), newSegment, ...trackList[id.track].slice(id.index + 1)],
+            ...trackList.slice(id.track + 1)
+        ]);
     }
 
     return (
